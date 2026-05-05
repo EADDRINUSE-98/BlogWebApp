e33fec21c21d93e3a530ed28f8958d184e3175bf
priyanshu <singhpriyanshu7827@gmail.com>
2026-05-05 23:27:43 +0530

    - started working on home page. - Trying to show a welcome message and just under it, have an upward   scrolling text. - This is gonna be hard but fuck it we ball. - Damn I hope nobody else see these commit messages and think I'm   autistic or something.

--- blogging_app/views/dashboard.py
+++ blogging_app/views/dashboard.py
@@ -39,5 +39,5 @@ def staff_check(user):
 @login_required()
 @user_passes_test(staff_check)
 def dashboard_view(request):
-    # return HttpResponse("This will be Dashboard")
-    return render(request, "dashboard/base.html")
+    context = {"full_name": request.user.get_full_name()}
+    return render(request, "dashboard/home.html", context)
