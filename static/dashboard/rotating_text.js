2251117ab967a6028a1789e966d79f6ec2e46571
priyanshu <singhpriyanshu7827@gmail.com>
2026-05-08 22:34:02 +0530

    - Nothing much today. - Just create a fading text rotation under "Hi <username>".

---
+++ static/dashboard/rotating_text.js
@@ -0,0 +1,26 @@
+const textArray = [
+  "&xlarr; Pick you poison!",
+  "Continue where you left off.",
+  "Put your thoughts on pixels.",
+  "Typing a day keeps the mediocrity at bay.",
+  "Gaining knowledge is good, but sharing it is even better."
+]
+let indx = 0
+
+const p = document.getElementById("rotating_text");
+
+const updateText = () => {
+  p.style.transform = "translateY(-20px)";
+  p.style.opacity = 0;
+
+  setTimeout(() => {
+    p.innerHTML = textArray[indx];
+    p.style.transform = "translateY(0)";
+    p.style.opacity = 1;
+    indx = (indx + 1) % textArray.length;
+  }, 500);
+}
+
+updateText();
+
+setInterval(updateText, 5000);
