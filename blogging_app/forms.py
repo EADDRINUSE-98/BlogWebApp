c69d6f6557fa209fce421dec0311af32ba58f4d2
priyanshu <singhpriyanshu7827@gmail.com>
2026-05-10 22:54:13 +0530

    - Completed UI side of the form for creating post. - Next, will start wiring up with the backend. - Shit's getting fun.

--- blogging_app/forms.py
+++ blogging_app/forms.py
@@ -8,7 +8,25 @@ class CreatePostForms(forms.ModelForm):
         fields = ["title", "description", "content", "is_published"]
 
         widgets = {
-            "title": forms.TextInput(attrs={"id": "title_input"}),
-            "description": forms.Textarea(attrs={"id": "description_input"}),
-            "content": forms.Textarea(attrs={"id": "content_input"}),
+            "title": forms.TextInput(
+                attrs={"id": "title_input", "placeholder": "Give a title to your post."}
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "id": "description_input",
+                    "placeholder": "Tell what this post is about in short.",
+                }
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "id": "content_input",
+                    "placeholder": "Explain your thoughts in detail.",
+                }
+            ),
+            "is_published": forms.Select(
+                attrs={
+                    "id": "status_input",
+                    "placeholder": "Select status of the post.",
+                }
+            ),
         }
