fae223f60ec8bc84da5b6308849ba6fdf64a4e90
priyanshu <singhpriyanshu7827@gmail.com>
2026-05-17 22:45:48 +0530

    - Implemented view logic for showing post in details. - Now home view will only show published post in the list. - A custom 403 forbiden page is also created.

--- blogging_app/views/public.py
+++ blogging_app/views/public.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from blogging_app.models import Post
 
-from django.http import HttpResponse
+# from django.views import generic
+from blogging_app.models import Post
+from django.core.exceptions import PermissionDenied
+# from django.http import HttpResponse
 # from .. import models
 
 # Create your public views here.
 
 
 def home(request):
-    blog_list = Post.objects.all().values()
+    blog_list = Post.objects.filter(is_published=True).values()
     context = {"blog_list": blog_list}
     return render(request, "public/home.html", context)
 
 
 def post_detail_view(request, slug):
     if request.method == "GET":
-        blog_details = get_object_or_404(Post, slug=slug)
-
-    return HttpResponse(f"This is post detail view with slug: {slug}")
-
-
-# class PostDetailPage(generic.DetailView):
-#     model = Post
-#     template_name = "blog_detail.html"
-#     context_object_name = "blog"
+        blog_details = get_object_or_404(Post, slug=slug, is_published=True)
+        context = {
+            "blog_title": blog_details.title,
+            "blog_publish_timestamp": blog_details.publish_timestamp,
+            "blog_content": blog_details.content,
+        }
+        return render(request, "public/post.html", context)
+    raise PermissionDenied
