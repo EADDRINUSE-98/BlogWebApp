b2831ad50b36cb1fcbfcfb03ab53d0164a355cfd
priyanshu <singhpriyanshu7827@gmail.com>
2026-06-30 22:59:16 +0530

    - Created and implemented a custom request rate limiter. - Tested the middleware with hey. - Results are documented in issue #2

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "request_rate_limiting.middleware.SimpleRateLimitor",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- request_rate_limiting/middleware.py
+++ request_rate_limiting/middleware.py
@@ -4,16 +4,14 @@ from django.core.cache import cache
 
 
 class SimpleRateLimitor(MiddlewareMixin):
-    def __init__(self):
-        super().__init__()
-        self.TIMEOUT_SEC = 60
-        self.REQ_LIMIT = 100
+    TIMEOUT_SEC = 60
+    REQ_LIMIT = 100
 
-    def get_response(self, request):
+    def process_request(self, request):
         """
         Rate limiting logic.
         """
-        ip = self.__get_request_origin_ip(request)
+        ip = self.get_request_origin_ip(request)
         key = f"rate_limit:{ip}"
         req_count = cache.get(
             key, 0
@@ -22,11 +20,11 @@ class SimpleRateLimitor(MiddlewareMixin):
             return JsonResponse({"error": "Request limit exceeded"}, status=429)
         cache.set(key, req_count + 1, timeout=self.TIMEOUT_SEC)
 
-    def __get_request_origin_ip(self, request):
+    def get_request_origin_ip(self, request):
         """
         Extracts origin IP from XFF header.
         """
-        xff_header = request.header.get("x-forwarded-for")
+        xff_header = request.headers.get("x-forwarded-for")
         if xff_header:
             ip_list = [ip.strip() for ip in xff_header.split(",")]
             return ip_list[0]
